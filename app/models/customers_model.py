67d2dab82d60c9ef4dddee23648b7eee3403e15c
fernandofrantz <fernandofrantz12@gmail.com>
2022-03-03 11:13:45 -0300

    feature: signin and signup implemented

--- app/models/customers_model.py
+++ app/models/customers_model.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from app.configs.database import db
 from sqlalchemy import Column, Integer, String, Boolean
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @dataclass
 class CustomerModel(db.Model):
@@ -18,4 +19,21 @@ class CustomerModel(db.Model):
     password_hash = Column(String, nullable=True)
     employee = Column(Boolean, nullable=False, default=False)
 
-    
+    def serializer(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password_hash": self.password_hash
+        }
+
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be accessed!")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def verify_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
