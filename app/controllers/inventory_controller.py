757360a45717960cf64e636a2951ffd6cbab6785
rodrigo-hardt <rodrigohardt@gmail.com>
2022-03-04 11:43:49 -0300

    feat: patch validations implemented at services

--- app/controllers/inventory_controller.py
+++ app/controllers/inventory_controller.py
@@ -1,3 +1,4 @@
+
 def get_inventory():
     return 'funciona', 200
 
--- migrations/versions/182f772ecbeb_.py
+++ migrations/versions/182f772ecbeb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5f9814576eb0
+Revision ID: 182f772ecbeb
 Revises: 
-Create Date: 2022-03-03 11:20:37.343343
+Create Date: 2022-03-04 11:40:45.511734
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5f9814576eb0'
+revision = '182f772ecbeb'
 down_revision = None
 branch_labels = None
 depends_on = None
